e0e23d4e2d31ee5e689a2c198a2f1ad9f2b042bb
Tudor Sandulescu <sekaru@sleepystudios.com>
2014-12-23 21:28:29 +0000

    Cleaned up Game class

--- src/net/indierising/momentum/Game.java
+++ src/net/indierising/momentum/Game.java
@@ -7,58 +7,31 @@ import org.newdawn.slick.SlickException;
 
 public class Game extends BasicGame{
 	static AppGameContainer app;
-	static int width=800,height=600;
 		
+	public Game(){
+		super("Test");
+	}
+	
+	public void init(GameContainer arg0) throws SlickException {
+		
+	}
+
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		
+	}
+
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		
+	}
+	
 	public static void main(String args[]) throws SlickException{
 		 app = new AppGameContainer(new Game());
 	     app.setShowFPS(false);
-	     app.setDisplayMode(width, height, false);
+	     app.setDisplayMode(1280, 720, false);
 	     app.setTargetFrameRate(60);
 	     app.setMaximumLogicUpdateInterval(10);
 		 app.setMaximumLogicUpdateInterval(60);
 	     app.setAlwaysRender(true);
 	     app.start();
-	}
-	
-	public Game(){
-		super("Test");
-	}
-
-	public void keyReleased(int key,char c){
-		
-	}	
-	
-	public void mouseMoved(int oldx,int oldy,int newx,int newy){
-
-	}
-
-	public void mousePressed(int button, int x, int y) {
-
-	}
-
-	public void mouseDragged(int oldx, int oldy, int newx, int newy){
-
-	}
-	
-	public void mouseReleased(int button, int x,int y){
-
-	}
-
-	@Override
-	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void init(GameContainer arg0) throws SlickException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void update(GameContainer arg0, int arg1) throws SlickException {
-		// TODO Auto-generated method stub
-		
 	}
 }
