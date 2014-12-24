2871f3542ea3624346356af8c9d6b17c710e5760
Mali Coxon <malii.coxon@gmail.com>
2014-12-24 12:22:27 +0000

    Changed the static publics to public statics *sigh

--- src/net/indierising/momentum/network/Reciever.java
+++ src/net/indierising/momentum/network/Reciever.java
@@ -6,7 +6,7 @@ import com.esotericsoftware.kryonet.Listener;
 public class Reciever extends Listener{
 	public void connected (Connection connection) {}
 	
-	public void received (Connection connection, final Object object) {}
+	public void received (Connection connection,Object object) {}
 	
 	public void disconnected (Connection connection){}
 }
