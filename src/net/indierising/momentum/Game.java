c86e1e32f1a22a91ebc7a6691fbaeade349c3173
Mali Coxon <malii.coxon@gmail.com>
2014-12-24 12:21:27 +0000

    Changed structure to statebasedgame

--- src/net/indierising/momentum/Game.java
+++ src/net/indierising/momentum/Game.java
@@ -4,34 +4,48 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
 
-public class Game extends BasicGame{
+
+public class Game extends StateBasedGame{
 	static AppGameContainer app;
-		
+	
+	// list of states
+	public static final int MAINMENUSTATE          	= 0;
+    public static final int INGAME          		= 1;
+    public final static int WIDTH 	= 1280;
+    public final static int HEIGHT 	= 720;
+	
 	public Game(){
-		super("Test");
+		super("Momentum Client");
+		this.addState(new Play(INGAME));
+        this.enterState(INGAME);
 	}
 	
-	public void init(GameContainer arg0) throws SlickException {
+	public void init(GameContainer gc,StateBasedGame sc) throws SlickException {
 		
 	}
 
-	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+	public void render(GameContainer gc,StateBasedGame sc, Graphics g) throws SlickException {
 		
 	}
 
-	public void update(GameContainer arg0, int arg1) throws SlickException {
+	public void update(GameContainer gc,StateBasedGame sc, int delta) throws SlickException {
 		
 	}
 	
 	public static void main(String args[]) throws SlickException{
 		 app = new AppGameContainer(new Game());
 	     app.setShowFPS(false);
-	     app.setDisplayMode(1280, 720, false);
+	     app.setDisplayMode(WIDTH, HEIGHT, false);
 	     app.setTargetFrameRate(60);
 	     app.setMaximumLogicUpdateInterval(10);
 		 app.setMaximumLogicUpdateInterval(60);
 	     app.setAlwaysRender(true);
 	     app.start();
+	}
+
+	public void initStatesList(GameContainer gc) throws SlickException {
+		this.getState(INGAME).init(gc, this);
 	}
 }
---
+++ src/net/indierising/momentum/Play.java
@@ -0,0 +1,33 @@
+package net.indierising.momentum;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Play extends BasicGameState{
+	// stores the stae of the game
+	int stateID = 0;
+	
+	public Play(int stateID){
+		this.stateID = stateID;
+	}
+	 
+	public int getID() {
+		return stateID;
+	}
+	
+	public void init(GameContainer gc,StateBasedGame sc) throws SlickException {
+		
+	}
+
+	public void render(GameContainer gc,StateBasedGame sc, Graphics g) throws SlickException {
+		
+	}
+
+	public void update(GameContainer gc,StateBasedGame sc, int delta) throws SlickException {
+		
+	}
+}
