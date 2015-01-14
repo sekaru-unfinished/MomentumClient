2595be2b3d70d7eaea43d3e5f4ff479b30db370c
Tudor Sandulescu <sekaru@sleepystudios.com>
2015-01-14 19:43:49 +0000

    Added Menu state skeleton

--- src/net/indierising/momentum/client/Menu.java
+++ src/net/indierising/momentum/client/Menu.java
@@ -1,5 +1,25 @@
 package net.indierising.momentum.client;
 
-public class Menu {
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
 
+public class Menu extends BasicGameState {
+	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+		
+	}
+
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+		
+	}
+
+	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+		
+	}
+
+	public int getID() {
+		return Game.MENU;
+	}
 }
